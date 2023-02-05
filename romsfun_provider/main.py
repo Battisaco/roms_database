de1a8c42457e3f42da610d6288d093486ac46616
Arthur Battisaco <art.battisaco@gmail.com>
2023-02-05 09:12:37 -0300

    Complete ROMFUN Scrapping and data saved

--- romsfun_provider/main.py
+++ romsfun_provider/main.py
@@ -1,8 +1,7 @@
-
-
 import os
 import util
-
+import test_util
+ 
 def run():
     url = "https://romsfun.com/roms"
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
@@ -12,7 +11,27 @@ def run():
 
     util.create_console_table(consoles,consoles_link)
 
-    game_link = util.get_game_link(consoles_link[0],headers)
+    util.console_loop(consoles_link,headers) 
 
+    
 if __name__ == "__main__":
     run()
+
+
+'''
+    game_link = ['https://romsfun.com/roms/game-boy/the-adventures-of-rocky-and-bullwinkle-and-friends.html',
+                 'https://romsfun.com/roms/gamecube/shrek-extra-large.html',
+                 'https://romsfun.com/roms/game-boy/the-legend-of-zelda-links-awakening.html',
+                 'https://romsfun.com/roms/gamecube/fifa-street-6.html'] 
+
+    temp = test_util.get_game_info_2(game_link[0],headers)
+    print(temp)
+    temp = test_util.get_game_info_2(game_link[1],headers)
+    print(temp)
+    temp = test_util.get_game_info_2(game_link[2],headers)
+    print(temp)
+    temp = test_util.get_game_info_2(game_link[3],headers)
+    print(temp)
+    
+
+'''
--- romsfun_provider/util.py
+++ romsfun_provider/util.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import os
+import time
+import test_util
 
 def get_consoles(url,headers):
 
@@ -35,25 +37,84 @@ def get_consoles_link(url,headers):
     return console_link
 
 def get_game_link(console_link,headers):
-
-    r = requests.get(console_link,headers=headers)
-    
-    soup = BeautifulSoup(r.content, 'html5lib')
-    raw = str(soup.find_all('a', class_="text-center bg-white border rounded shadow-sm d-flex flex-column h-100 p-3")) 
-
-    indexes = [m.start() for m in re.finditer('href=', raw)]
     game_link = []
-    
-    for index in indexes:
-        temp = raw[index+6:index+200].split('"')
-        game_link.append(temp[0])
+    lastpage = False
+    page = 1
+    print(console_link)
+    while(lastpage == False):
 
+        r = requests.get(console_link+f'/page/{page}',headers=headers)
+        
+        soup = BeautifulSoup(r.content, 'html5lib')
+        raw = str(soup.find_all('a', class_="text-center bg-white border rounded shadow-sm d-flex flex-column h-100 p-3")) 
+        indexes = [m.start() for m in re.finditer('href=', raw)]
+        
+        if len(indexes)<1:
+            print('ĹastPage, moving to next console')
+            print('----------------')
+            print()
+            return game_link
+        
+        for index in indexes:
+            temp = raw[index+6:index+200].split('"')
+            game_link.append(temp[0])
+        page+=1
     return game_link
 
+
+
+def create_game_table(game_link,headers):
+    
+
+    for aux_game_link in game_link:
+        df = pd.DataFrame(columns=['name', 'console', 
+                        'genre','region','publisher',
+                        'views','download','released','rate',
+                        'site','link'])
+        count = 1
+        flag = True
+        for link in aux_game_link:
+
+            table = {}
+            
+            info = test_util.get_game_info(link,headers)
+            print(f'Jogo {count} de {len(aux_game_link)}')
+            
+            if flag == True:
+                console_name = info[2]
+                if console_name != 'None':
+                    flag = False
+            count+=1
+
+            table['name']      = info[0]
+            table['rate']      = info[1]
+            table['console']   = info[2]
+            table['publisher'] = info[3]
+            table['genre']     = ','.join(info[4])
+            table['region']    = info[5]
+            table['views']     = info[6]
+            table['download']  = info[7]
+            table['released']  = info[8]
+            
+            table['site'] = 'ROMSFUN.com'
+            table['link'] = link
+
+            temp = pd.DataFrame(table, index=[0])
+            df = pd.concat([df,temp], ignore_index=True)
+        df.to_csv(os.path.join(os.path.dirname(__file__), f'game_tables/games_table_{console_name}.csv'),index=False)
+        
 def create_console_table(console,console_link):
     temp = {'Console':console,
-            'Link':console_link}
+            'Link':console_link,
+            'site':'ROMSFUN.com'}
 
     df = pd.DataFrame(temp)
     df.to_csv(os.path.join(os.path.dirname(__file__), 'Console_table.csv'),index=False)
-    
+
+def console_loop(console_link,headers):
+    game_links = []
+    for aux_console_link in console_link:
+        print(f'Start get game_link for {aux_console_link}')
+        game_links.append(get_game_link(aux_console_link,headers))
+
+    create_game_table(game_links,headers)
