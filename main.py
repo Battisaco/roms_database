f61b0c44fdf7edd6e206dcf466ea6a6db8915c8c
Arthur Battisaco <art.battisaco@gmail.com>
2023-02-05 12:14:16 -0300

    remove desnecessary comments

--- main.py
+++ main.py
@@ -6,21 +6,3 @@ def run():
 if __name__ == "__main__":
     run()
 
-
-'''
-    game_link = ['https://romsfun.com/roms/game-boy/the-adventures-of-rocky-and-bullwinkle-and-friends.html',
-                 'https://romsfun.com/roms/gamecube/shrek-extra-large.html',
-                 'https://romsfun.com/roms/game-boy/the-legend-of-zelda-links-awakening.html',
-                 'https://romsfun.com/roms/gamecube/fifa-street-6.html'] 
-
-    temp = test_util.get_game_info_2(game_link[0],headers)
-    print(temp)
-    temp = test_util.get_game_info_2(game_link[1],headers)
-    print(temp)
-    temp = test_util.get_game_info_2(game_link[2],headers)
-    print(temp)
-    temp = test_util.get_game_info_2(game_link[3],headers)
-    print(temp)
-    
-
-'''
