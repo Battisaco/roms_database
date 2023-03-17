9e82af5e5325097389ca11d4d9e9739b8c4a98e6
Battisaco <art.battisaco@gmail.com>
2023-03-17 12:29:18 -0300

    added functions

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import random
+
 def str_to_int(s: str) -> int:
     """Converts a string to an integer."""
     return int(s.strip().replace(",", ""))
@@ -6,3 +8,21 @@ def str_to_int(s: str) -> int:
 def str_to_float(s: str) -> float:
     """Converts a string to a float."""
     return float(s.strip())
+
+
+def random_header():
+    '''
+    Get a random header for every consult, in this way avoid getting the 
+    ip banned
+    '''
+    user_agent_list = [ 
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246', 
+	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36', 
+	'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148', 
+	'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Mobile Safari/537.36',
+    ]
+    user_agent = random.choice(user_agent_list)
+
+    return user_agent  
